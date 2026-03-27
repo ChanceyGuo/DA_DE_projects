5130b52cd283269b35bea0c8124bc61d0ee48112
ChanceyGuo <ChanceyGuo@outlook.com>
2026-03-27 18:17:55 +0800

    build a compete and repeatable ETL pipeline with log with webscraping

---
+++ ETL_pipeline_country/etl_project_gdp.py
@@ -0,0 +1,94 @@
+# Importing libraries
+
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+import sqlite3
+from datetime import datetime 
+
+# Initialize entities
+
+url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+table_attribs = ["Country", "GDP_USD_millions"]
+db_name = 'World_Economies.db'
+table_name = 'Countries_by_GDP'
+csv_path = './Countries_by_GDP.csv'
+
+# Functions 
+
+def extract(url, table_attribs):
+    row_list = []
+    page = requests.get(url).text
+    data = BeautifulSoup(page,'html.parser')
+    tables = data.find_all('tbody')
+    rows = tables[2].find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        if len(cols) != 0:
+            if cols[0].find('a') is not None and '—' not in cols[2]:
+                dict_a = {'Country':cols[0].a.contents[0], 'GDP_USD_millions':cols[2].contents[0]}
+                row_list.append(dict_a)
+    df = pd.DataFrame(row_list,columns = table_attribs)
+    return df
+
+
+def transform(df):
+    clean_GDP_list = []
+    GDP_list = df['GDP_USD_millions'].tolist()
+    for x in GDP_list:
+        nums = float (x.replace(',', ''))
+        nums = np.round(nums/1000,2)
+        clean_GDP_list.append(nums)
+    df["GDP_USD_millions"] = clean_GDP_list
+    df=df.rename(columns = {"GDP_USD_millions":"GDP_USD_billions"})
+    return df
+
+def load_to_csv(df, csv_path):
+	df.to_csv(csv_path)
+
+def load_to_db(df, sql_connection, table_name):
+	df.to_sql(table_name,sql_connection, if_exists = 'replace', index = False)
+
+def run_query(query_statement, sql_connection):
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
+
+
+def log_progress(message): 
+    timestamp = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now() 
+    timestamp = now.strftime(timestamp) 
+    with open("./etl_project_log.txt","a") as f: 
+        f.write(timestamp + ' : ' + message + '\n')
+	
+# Main
+log_progress('Initiating ETL process')
+
+df = extract(url, table_attribs)
+
+log_progress('Data extraction complete. Initiating Transformation process')
+
+df = transform(df)
+
+log_progress('Data transformation complete. Initiating loading process')
+
+load_to_csv(df, csv_path)
+
+log_progress('Data saved to CSV file')
+
+sql_connection = sqlite3.connect('World_Economies.db')
+
+log_progress('SQL Connection initiated.')
+
+load_to_db(df, sql_connection, table_name)
+
+log_progress('Data loaded to Database as table. Running the query')
+
+query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+run_query(query_statement, sql_connection)
+
+log_progress('Process Complete.')
+
+sql_connection.close()
